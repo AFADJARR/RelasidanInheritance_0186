098adced8990dd0574120e5de213e158940c7e30
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-26 16:52:44 +0700

    first commit

---
+++ Inheritance/inerhitance.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang
+{
+public:
+    string nama;
+
+    orang(string pNama) : nama(pNama)
+    {
+        cout << "orang dibuat\n"
+             << endl;
+    }
+    ~orang()
+    {
+        cout << "orang dihapus\n"
+             << endl;
+    }
+
+    int jumlah(int a, int b)
+    {
+        return a + b;
+    }
+};
+
+class pelajar : public orang
+{
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah)
+    {
+        cout << "pelajar dibuat\n"
+             << endl;
+    }
+    ~pelajar()
+    {
+        cout << "pelajar dihapus\n"
+             << endl;
+    }
+    string perkenalan()
+    {
+        return "Halo, nama saya" + nama + "dari sekolah" + sekolah + "\n\n";
+    }
+};
+
+int main()
+{
+    pelajar siswa1("andi laksono", "SMAN 1 Bantu1");
+    cout << siswa1.perkenalan();
+    cout << "Hasi1 = " << siswa1.jumlah(10, 90) << endl;
+    return 0;
+}
